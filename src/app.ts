64abcb2ffbd5bcd216e3c21f64a65072d9035a4b
ohidurgclan <ohidur.ru.2019@gmail.com>
2026-01-30 06:02:31 +0600

    Middleware Added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
- import express, { Application } from "express";
+ import express, { Application, Request, Response } from "express";
 import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import { auth } from "./lib/auth";
 
 const app: Application = express();
 app.use(cors({
-    origin: process.env.APP_URL || "http://localhost:3000", // allow to server to accept requests from this
-    credentials: true // allow session cookie from browser to pass through
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true 
 }))
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res: Response) => {
     res.send("FoodHub Backend is running... Congratulations! Md Ohidur Rahman");
 });
 
